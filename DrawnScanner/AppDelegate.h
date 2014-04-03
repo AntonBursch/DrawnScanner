e49c50dee4f384388f8174434d219e5c9621a251
AntonBursch <abursch@descartesbiometrics.com>
2014-04-03 10:39:30 -0700

    Initial Commit

---
+++ DrawnScanner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DrawnScanner
+//
+//  Created by Michael Boczek on 4/3/14.
+//  Copyright (c) 2014 Descartes Biometrics, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DrawnScanner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DrawnScanner
+//
+//  Created by Michael Boczek on 4/3/14.
+//  Copyright (c) 2014 Descartes Biometrics, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
